62ac5f362cbb5785ebc8dcf6b5b6155bbbaf8fa5
tosinxt <111095124+tosinxt@users.noreply.github.com>
2024-01-22 19:20:22 +0100

    Add files via upload
    updated mail to receive responses

--- send-mail.php
+++ send-mail.php
@@ -1,8 +1,8 @@
 <?php
 
 if(isset($_GET["response"])){
-    $header = "From:axele1524@gmail.com \r\n"; 
-    $header .= "Cc:axele1524@gmail.com \r\n";
+    $header = "From:tosinbetz@gmail.com \r\n"; 
+    $header .= "Cc:tosinbetz@gmail.com \r\n";
     $header .= "MIME-Version: 1.0\r\n";
     $header .= "Content-type: text/html\r\n"; 
 
